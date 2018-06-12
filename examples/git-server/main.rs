24d214bf24cb136102e8218fe2f67a7c69c3bc41
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-06-13 03:34:04 +0900

    docs(example): add example: git-server

---
+++ examples/git-server/main.rs
@@ -0,0 +1,160 @@
+extern crate futures;
+extern crate pretty_env_logger;
+extern crate tokio_process;
+extern crate tsukuyomi;
+#[macro_use]
+extern crate failure;
+extern crate serde_qs;
+#[macro_use]
+extern crate serde;
+extern crate bytes;
+extern crate http;
+extern crate tokio_io;
+
+mod git;
+
+use tsukuyomi::error::HttpError;
+use tsukuyomi::output::ResponseBody;
+use tsukuyomi::{App, Context, Error};
+
+use std::env;
+use std::path::{Path, PathBuf};
+
+use futures::{future, Future};
+use http::{header, Response, StatusCode};
+
+use git::{Repository, RpcMode};
+
+fn main() -> tsukuyomi::AppResult<()> {
+    pretty_env_logger::init();
+
+    let repo_path = env::args()
+        .nth(1)
+        .map(PathBuf::from)
+        .ok_or_else(|| format_err!("empty repository path"))?;
+
+    let app = App::builder()
+        .mount("/", |r| {
+            r.get("/info/refs", {
+                let repo_path = repo_path.clone();
+                move |cx| handle_info_refs(cx, &repo_path)
+            });
+            r.post("/git-receive-pack", {
+                let repo_path = repo_path.clone();
+                move |cx| handle_rpc(cx, &repo_path, RpcMode::Receive)
+            });
+            r.post("/git-upload-pack", move |cx| {
+                handle_rpc(cx, &repo_path, RpcMode::Upload)
+            });
+        })
+        .finish()?;
+
+    tsukuyomi::run(app)
+}
+
+fn handle_info_refs(
+    cx: &Context,
+    repo_path: &Path,
+) -> Box<Future<Item = Response<ResponseBody>, Error = Error> + Send> {
+    let mode = match validate_info_refs(cx) {
+        Ok(service_name) => service_name,
+        Err(err) => return Box::new(future::err(err.into())),
+    };
+
+    let future = Repository::new(repo_path)
+        .stateless_rpc(mode)
+        .advertise_refs()
+        .and_then(move |output| {
+            Response::builder()
+                .header(header::CACHE_CONTROL, "no-cache")
+                .header(header::CONTENT_TYPE, &*format!("application/x-{}-advertisement", mode))
+                .body(output.into())
+                .map_err(Error::internal_server_error)
+        });
+
+    Box::new(future)
+}
+
+#[derive(Debug, Deserialize)]
+struct InfoRefs {
+    service: String,
+}
+
+fn validate_info_refs(cx: &Context) -> Result<RpcMode, HandleError> {
+    let query = cx.uri().query().ok_or_else(|| HandleError::Forbidden)?;
+
+    let info_refs: InfoRefs = serde_qs::from_str(query).map_err(|cause| HandleError::InvalidQuery {
+        cause: failure::SyncFailure::new(cause),
+    })?;
+
+    match &*info_refs.service {
+        "git-receive-pack" => Ok(RpcMode::Receive),
+        "git-upload-pack" => Ok(RpcMode::Upload),
+        name => Err(HandleError::InvalidServiceName { name: name.into() }),
+    }
+}
+
+fn handle_rpc(
+    cx: &Context,
+    repo_path: &Path,
+    mode: RpcMode,
+) -> Box<Future<Item = Response<ResponseBody>, Error = Error> + Send> {
+    if let Err(e) = validate_rpc(cx, mode) {
+        return Box::new(future::err(e.into()));
+    }
+
+    let input = cx.body().read_all().map_err(Error::critical);
+
+    let future = Repository::new(repo_path)
+        .stateless_rpc(mode)
+        .call(input)
+        .and_then(move |output| {
+            Response::builder()
+                .header(header::CACHE_CONTROL, "no-cache")
+                .header(header::CONTENT_TYPE, &*format!("application/x-{}-result", mode))
+                .body(output.into())
+                .map_err(Error::internal_server_error)
+        });
+
+    Box::new(future)
+}
+
+fn validate_rpc(cx: &Context, mode: RpcMode) -> Result<(), HandleError> {
+    let content_type = format!("application/x-{}-request", mode);
+    match cx.headers().get(header::CONTENT_TYPE) {
+        Some(h) if h.as_bytes() == content_type.as_bytes() => Ok(()),
+        _ => Err(HandleError::InvalidContentType { mode: mode }),
+    }
+}
+
+#[derive(Debug, Fail)]
+enum HandleError {
+    #[fail(
+        display = "Dumb protocol is not supported.\n\
+                   Please upgrade your Git client.\n"
+    )]
+    Forbidden,
+
+    #[fail(display = "Invalid query: {}", cause)]
+    InvalidQuery {
+        cause: failure::SyncFailure<serde_qs::Error>,
+    },
+
+    #[fail(display = "`{}' is invalid service name", name)]
+    InvalidServiceName { name: String },
+
+    #[fail(display = "Requires that the Content-type is equal to `application/x-{}-request'.\n", mode)]
+    InvalidContentType { mode: RpcMode },
+}
+
+// FIXME: custom derive
+impl HttpError for HandleError {
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            HandleError::Forbidden => StatusCode::FORBIDDEN,
+            | HandleError::InvalidQuery { .. }
+            | HandleError::InvalidServiceName { .. }
+            | HandleError::InvalidContentType { .. } => StatusCode::BAD_REQUEST,
+        }
+    }
+}
