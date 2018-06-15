4ad3ecb897b27d1e7ea95a12a876da9f680eb151
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-06-16 01:45:24 +0900

    docs(example): add example using Askama template engine

---
+++ examples/template-askama/src/main.rs
@@ -0,0 +1,48 @@
+extern crate tsukuyomi;
+#[macro_use]
+extern crate askama;
+extern crate failure;
+extern crate http;
+
+use askama::Template as _Template;
+use failure::SyncFailure;
+use http::{header, Response};
+use tsukuyomi::future::{ready, Ready};
+use tsukuyomi::output::{Output, Responder};
+use tsukuyomi::{App, Context, Error};
+
+struct Template<T: _Template>(T);
+
+impl<T: _Template> Responder for Template<T> {
+    fn respond_to(self, _: &Context) -> Result<Output, Error> {
+        let body = self.0
+            .render()
+            .map_err(|e| Error::internal_server_error(SyncFailure::new(e)))?;
+        Response::builder()
+            .header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+            .body(body)
+            .map(Into::into)
+            .map_err(Error::internal_server_error)
+    }
+}
+
+#[derive(Debug, Template)]
+#[template(path = "index.html")]
+struct Hello {
+    name: String,
+}
+
+fn index(cx: &Context) -> Ready<Template<Hello>> {
+    let name = cx.params()[0].to_owned();
+    ready(Template(Hello { name: name }))
+}
+
+fn main() -> tsukuyomi::AppResult<()> {
+    let app = App::builder()
+        .mount("/", |r| {
+            r.get("/:name", index);
+        })
+        .finish()?;
+
+    tsukuyomi::run(app)
+}
