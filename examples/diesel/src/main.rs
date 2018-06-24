8b7eee97a05f3a2edb14d69453414df05bd10376
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-06-25 03:08:26 +0900

    docs(example): update diesel example

--- examples/diesel/src/main.rs
+++ examples/diesel/src/main.rs
@@ -3,8 +3,11 @@ extern crate diesel;
 extern crate dotenv;
 extern crate failure;
 extern crate futures;
+extern crate http;
 #[macro_use]
 extern crate serde;
+extern crate pretty_env_logger;
+extern crate serde_qs;
 extern crate tokio_executor;
 extern crate tokio_threadpool;
 extern crate tsukuyomi;
@@ -19,14 +22,17 @@ use std::env;
 use tsukuyomi::{App, Handler};
 
 fn main() -> tsukuyomi::AppResult<()> {
+    pretty_env_logger::init();
     dotenv()?;
 
     let pool = conn::init_pool(env::var("DATABASE_URL")?)?;
 
     let app = App::builder()
         .manage(pool)
-        .mount("/posts", |r| {
-            r.get("/").handle(Handler::new_async(api::get_posts));
+        .mount("/api/v1/posts", |m| {
+            m.get("/").handle(Handler::new_async(api::get_posts));
+            m.post("/").handle(Handler::new_async(api::create_post));
+            m.get("/:id").handle(Handler::new_async(api::get_post));
         })
         .finish()?;
 
