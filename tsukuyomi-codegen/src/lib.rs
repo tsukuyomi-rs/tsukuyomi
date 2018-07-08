549ba2ddf276cdc73e4a337326d118d704bb1d75
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-07-08 09:45:40 +0900

    refactor tsukuyomi-codegen

--- tsukuyomi-codegen/src/lib.rs
+++ tsukuyomi-codegen/src/lib.rs
@@ -26,42 +26,6 @@ macro_rules! try_quote {
     };
 }
 
-macro_rules! bail_quote {
-    ($e:expr) => {{
-        use proc_macro2::Span;
-        use quote::*;
-        let msg = $e.to_string();
-        return Into::into(quote_spanned!(Span::call_site() => compile_error!(#msg)));
-    }};
-    ($e:expr, $($args:expr),*) => {{
-        bail_quote!(format!($e, $($args),*))
-    }}
-}
-
-#[derive(Debug, Copy, Clone, PartialEq)]
-enum HandlerMode {
-    Ready,
-    Async,
-    AsyncAwait,
-}
-
-impl std::str::FromStr for HandlerMode {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.trim() {
-            "" | "ready" => Ok(HandlerMode::Ready),
-            "async" => Ok(HandlerMode::Async),
-            "await" => Ok(HandlerMode::AsyncAwait),
-            s => Err(format!("invalid mode: `{}'", s)),
-        }
-    }
-}
-
-fn detect_mode(attr: &TokenStream, _item: &syn::ItemFn) -> Result<HandlerMode, String> {
-    attr.to_string().parse()
-}
-
 /// Modifies the signature of a free-standing function to a suitable form as handler function.
 ///
 /// This macro generates a handler function with inserting some processes before
@@ -135,70 +99,132 @@ fn detect_mode(attr: &TokenStream, _item: &syn::ItemFn) -> Result<HandlerMode, S
 #[proc_macro_attribute]
 pub fn handler(attr: TokenStream, item: TokenStream) -> TokenStream {
     let item: syn::ItemFn = try_quote!(syn::parse(item));
-
     // FIXME: detect the keyword `async`
     let mode = try_quote!(detect_mode(&attr, &item));
 
-    let num_args = item.decl.inputs.iter().count();
-    if num_args > 1 {
-        bail_quote!("Too many arguments");
-    }
-    if mode == HandlerMode::AsyncAwait && num_args != 0 {
-        bail_quote!("The number of arguments in #[async] handler must be zero.");
-    }
-
-    let mut inner = item.clone();
-    inner.ident = syn::Ident::new("inner", inner.ident.span());
-    if mode == HandlerMode::AsyncAwait {
-        inner.attrs.push(parse_quote!(#[async]));
-    }
-
-    let mut new_item = item.clone();
-    let input_ident: syn::Ident = if num_args == 0 && mode != HandlerMode::Ready {
-        syn::Ident::new("_input", Span::call_site())
-    } else {
-        syn::Ident::new("input", Span::call_site())
+    let context = Context {
+        item: item,
+        mode: mode,
     };
-    new_item.decl.inputs = Some(syn::punctuated::Pair::End(parse_quote!(
-        #input_ident: &mut ::tsukuyomi::input::Input
-    ))).into_iter()
-        .collect();
-    match new_item.decl.output {
-        syn::ReturnType::Default => bail_quote!("unimplemented"),
-        syn::ReturnType::Type(_, ref mut ty) => {
-            *ty = Box::new(parse_quote!(::tsukuyomi::handler::Handle));
-        }
-    }
-    new_item.block = {
-        let call: syn::Expr = match num_args {
-            0 => parse_quote!(inner()),
-            1 => parse_quote!(inner(input)),
-            _ => unreachable!(),
-        };
 
-        let body: syn::Expr = match mode {
-            HandlerMode::Ready => parse_quote!({
-                ::tsukuyomi::handler::Handle::ready(
-                    ::tsukuyomi::output::Responder::respond_to(#call, input)
-                )
-            }),
-            HandlerMode::Async | HandlerMode::AsyncAwait => parse_quote!({
-                ::tsukuyomi::handler::Handle::async_responder(#call)
-            }),
-        };
-
-        let prelude: Option<syn::Stmt> = if mode == HandlerMode::AsyncAwait {
-            Some(parse_quote!(use futures::prelude::async;))
-        } else {
-            None
-        };
-
-        Box::new(parse_quote!({
-            #prelude
-            #inner
-            #body
-        }))
-    };
+    try_quote!(context.validate());
+    let inner = try_quote!(context.generate_inner());
+    let new_item = try_quote!(context.generate_new_item(inner));
 
     quote!(#new_item).into()
+}
+
+// ====
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum HandlerMode {
+    Ready,
+    Async,
+    AsyncAwait,
+}
+
+impl std::str::FromStr for HandlerMode {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim() {
+            "" | "ready" => Ok(HandlerMode::Ready),
+            "async" => Ok(HandlerMode::Async),
+            "await" => Ok(HandlerMode::AsyncAwait),
+            s => Err(format!("invalid mode: `{}'", s)),
+        }
+    }
+}
+
+fn detect_mode(attr: &TokenStream, _item: &syn::ItemFn) -> Result<HandlerMode, String> {
+    attr.to_string().parse()
+}
+
+struct Context {
+    mode: HandlerMode,
+    item: syn::ItemFn,
+}
+
+impl Context {
+    fn num_inputs(&self) -> usize {
+        self.item.decl.inputs.len()
+    }
+
+    fn validate(&self) -> Result<(), Box<std::fmt::Display>> {
+        if self.num_inputs() > 1 {
+            return Err(Box::new("Too many arguments"));
+        }
+
+        if self.mode == HandlerMode::AsyncAwait && self.num_inputs() != 0 {
+            return Err(Box::new("The number of arguments in #[async] handler must be zero."));
+        }
+
+        Ok(())
+    }
+
+    fn generate_inner(&self) -> Result<syn::ItemFn, Box<std::fmt::Display>> {
+        let mut inner = self.item.clone();
+
+        inner.ident = syn::Ident::new("inner", Span::call_site());
+
+        if self.mode == HandlerMode::AsyncAwait {
+            inner.attrs.push(parse_quote!(#[async]));
+        }
+
+        Ok(inner)
+    }
+
+    fn generate_new_item(&self, inner: syn::ItemFn) -> Result<syn::ItemFn, Box<std::fmt::Display>> {
+        let mut new_item = self.item.clone();
+
+        let input_ident: syn::Ident = if self.num_inputs() == 0 && self.mode != HandlerMode::Ready {
+            syn::Ident::new("_input", Span::call_site())
+        } else {
+            syn::Ident::new("input", Span::call_site())
+        };
+        new_item.decl.inputs = Some(syn::punctuated::Pair::End(parse_quote!(
+            #input_ident: &mut ::tsukuyomi::input::Input
+        ))).into_iter()
+            .collect();
+
+        match new_item.decl.output {
+            syn::ReturnType::Default => unimplemented!(),
+            syn::ReturnType::Type(_, ref mut ty) => {
+                *ty = Box::new(parse_quote!(::tsukuyomi::handler::Handle));
+            }
+        }
+
+        new_item.block = {
+            let call: syn::Expr = match self.num_inputs() {
+                0 => parse_quote!(inner()),
+                1 => parse_quote!(inner(input)),
+                _ => unreachable!(),
+            };
+
+            let body: syn::Expr = match self.mode {
+                HandlerMode::Ready => parse_quote!({
+                    ::tsukuyomi::handler::Handle::ready(
+                        ::tsukuyomi::output::Responder::respond_to(#call, input)
+                    )
+                }),
+                HandlerMode::Async | HandlerMode::AsyncAwait => parse_quote!({
+                    ::tsukuyomi::handler::Handle::async_responder(#call)
+                }),
+            };
+
+            let prelude: Option<syn::Stmt> = if self.mode == HandlerMode::AsyncAwait {
+                Some(parse_quote!(use futures::prelude::async;))
+            } else {
+                None
+            };
+
+            Box::new(parse_quote!({
+                #prelude
+                #inner
+                #body
+            }))
+        };
+
+        Ok(new_item)
+    }
 }
