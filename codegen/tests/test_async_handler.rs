a8a5d3054285705291c20aed897c0f3c9b41154d
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-07-26 17:44:34 +0900

    reform tsukuyomi-codegen

---
+++ codegen/tests/test_async_handler.rs
@@ -0,0 +1,22 @@
+#![feature(use_extern_macros)]
+
+extern crate futures;
+extern crate tsukuyomi;
+extern crate tsukuyomi_codegen;
+
+use tsukuyomi::{AsyncResponder, Error, Handler, Input};
+use tsukuyomi_codegen::async_handler;
+
+fn assert_impl<T: Handler>(t: T) {
+    drop(t);
+}
+
+#[async_handler]
+fn handler(_: &mut Input) -> impl AsyncResponder {
+    futures::future::ok::<_, Error>("Hello")
+}
+
+#[test]
+fn main() {
+    assert_impl(handler);
+}
