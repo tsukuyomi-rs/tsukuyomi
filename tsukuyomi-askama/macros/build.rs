1bbbc6799d69cdebfe1b93fa1b1d4df1e4a53ff1
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-11-04 19:10:06 +0900

    update tsukuyomi-askama

--- tsukuyomi-askama/macros/build.rs
+++
@@ -1,7 +0,0 @@
-use std::env;
-
-fn main() {
-    if env::var_os("TSUKUYOMI_DENY_WARNINGS").is_some() {
-        println!("cargo:rustc-cfg=tsukuyomi_deny_warnings");
-    }
-}
--- tsukuyomi-askama/tests/askama.rs
+++ tsukuyomi-askama/tests/askama.rs
@@ -2,20 +2,26 @@ extern crate askama;
 extern crate tsukuyomi;
 extern crate tsukuyomi_askama;
 
+use askama::Template;
+use tsukuyomi::app::App;
+use tsukuyomi::route;
+use tsukuyomi_askama::TemplateResponder;
+
+#[inline]
+fn assert_impl<T: TemplateResponder>(x: T) -> T {
+    x
+}
+
 #[test]
 fn test_template() {
-    use askama::Template;
-    use tsukuyomi::app::App;
-    use tsukuyomi::route;
-
-    #[derive(Template, tsukuyomi_askama::Responder)]
+    #[derive(Template, TemplateResponder)]
     #[template(path = "index.html")]
     struct Index {
         name: &'static str,
     }
 
     let app = App::builder()
-        .route(route::index().reply(|| Index { name: "Alice" }))
+        .route(route::index().reply(|| assert_impl(Index { name: "Alice" })))
         .finish()
         .unwrap();
     drop(app);
