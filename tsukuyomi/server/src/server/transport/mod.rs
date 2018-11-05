db457242dd5711bc2573ed3b27ba9a6a7534b009
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-11-05 16:58:11 +0900

    impl Transport for &str/String

--- tsukuyomi/server/src/server/transport/mod.rs
+++ tsukuyomi/server/src/server/transport/mod.rs
@@ -27,6 +27,32 @@ mod imp {
         fn incoming(self) -> io::Result<Self::Incoming>;
     }
 
+    impl Transport for String {}
+    impl TransportImpl for String {
+        type Item = tokio::net::TcpStream;
+        type Error = std::io::Error;
+        type Incoming = tokio::net::tcp::Incoming;
+
+        #[inline]
+        fn incoming(self) -> std::io::Result<Self::Incoming> {
+            self.as_str().incoming()
+        }
+    }
+
+    impl<'a> Transport for &'a str {}
+    impl<'a> TransportImpl for &'a str {
+        type Item = tokio::net::TcpStream;
+        type Error = std::io::Error;
+        type Incoming = tokio::net::tcp::Incoming;
+
+        #[inline]
+        fn incoming(self) -> std::io::Result<Self::Incoming> {
+            self.parse::<std::net::SocketAddr>()
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?
+                .incoming()
+        }
+    }
+
     impl Transport for SocketAddr {}
     impl TransportImpl for SocketAddr {
         type Item = tokio::net::TcpStream;
