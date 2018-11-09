0b042f313b3a8f7092456736aafcd5f5687ef55f
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-11-09 17:18:38 +0900

    update http-proxy

--- examples/http-proxy/src/main.rs
+++ examples/http-proxy/src/main.rs
@@ -1,63 +1,33 @@
 #![cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
 
-extern crate bytes;
 extern crate futures;
 extern crate http;
 extern crate reqwest;
 extern crate tsukuyomi;
 
-use std::mem;
+mod proxy;
 
-use bytes::{Buf, Bytes};
+use crate::proxy::Client;
 use futures::prelude::*;
-use reqwest::async::Client;
-
-use tsukuyomi::error::Error;
-use tsukuyomi::extractor;
-use tsukuyomi::output::Responder;
-
-fn index(client: Client) -> impl Future<Item = impl Responder, Error = Error> {
-    client
-        .get("http://www.example.com")
-        .send()
-        .and_then(|mut resp| {
-            let mut response = http::Response::new(());
-            *response.status_mut() = resp.status();
-            mem::swap(response.headers_mut(), resp.headers_mut());
-            resp.into_body()
-                .concat2()
-                .map(move |chunks| response.map(|_| chunks.collect::<Bytes>()))
-        }).map_err(tsukuyomi::error::internal_server_error)
-}
-
-fn streaming(client: Client) -> impl Future<Item = impl Responder, Error = Error> {
-    client
-        .get("https://www.rust-lang.org/en-US/")
-        .send()
-        .map(|mut resp| {
-            let mut response = http::Response::new(());
-            *response.status_mut() = resp.status();
-            mem::swap(response.headers_mut(), resp.headers_mut());
-            response.map(|_| tsukuyomi::output::ResponseBody::wrap_stream(resp.into_body()))
-        }).map_err(tsukuyomi::error::internal_server_error)
-}
 
 fn main() {
-    let app = tsukuyomi::app(|scope| {
-        scope.state(Client::new());
+    let proxy_client =
+        std::sync::Arc::new(crate::proxy::proxy_client(reqwest::async::Client::new()));
 
-        scope.route(
-            tsukuyomi::route("/")
-                .with(extractor::state::cloned())
-                .handle(index),
-        );
+    let app =
+        tsukuyomi::app(|scope| {
+            scope.route(tsukuyomi::route("/").with(proxy_client.clone()).handle(
+                |client: Client| {
+                    client
+                        .send_forwarded_request("http://www.example.com")
+                        .and_then(|resp| resp.receive_all())
+                },
+            ));
 
-        scope.route(
-            tsukuyomi::route("/streaming")
-                .with(extractor::state::cloned())
-                .handle(streaming),
-        );
-    }).unwrap();
+            scope.route(tsukuyomi::route("/streaming").with(proxy_client).handle(
+                |client: Client| client.send_forwarded_request("https://www.rust-lang.org/en-US/"),
+            ));
+        }).unwrap();
 
     tsukuyomi::server(app).run_forever().unwrap();
 }
---
+++ examples/http-proxy/src/proxy.rs
@@ -0,0 +1,109 @@
+use std::mem;
+use std::net::SocketAddr;
+
+use futures::prelude::*;
+use http::header::{Entry, HeaderMap};
+use reqwest::IntoUrl;
+
+use tsukuyomi::error::{Error, Never};
+use tsukuyomi::extractor;
+use tsukuyomi::extractor::{Extractor, ExtractorExt};
+use tsukuyomi::input::Input;
+use tsukuyomi::output::Responder;
+
+#[derive(Debug)]
+pub struct Client {
+    client: reqwest::async::Client,
+    headers: HeaderMap,
+    peer_addr: SocketAddr,
+}
+
+impl Client {
+    pub fn send_forwarded_request(
+        self,
+        url: impl IntoUrl,
+    ) -> impl Future<Error = Error, Item = ProxyResponse> {
+        let Self {
+            client,
+            mut headers,
+            peer_addr,
+        } = self;
+
+        headers.remove("host");
+
+        match headers
+            .entry("x-forwarded-for")
+            .expect("should be a valid header name")
+        {
+            Entry::Occupied(mut entry) => {
+                let addrs = format!("{}, {}", entry.get().to_str().unwrap(), peer_addr);
+                entry.insert(addrs.parse().unwrap());
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(peer_addr.to_string().parse().unwrap());
+            }
+        }
+
+        client
+            .get(url)
+            .headers(headers)
+            .send()
+            .map(|resp| ProxyResponse { resp })
+            .map_err(tsukuyomi::error::internal_server_error)
+    }
+}
+
+pub struct ProxyResponse {
+    resp: reqwest::async::Response,
+}
+
+impl ProxyResponse {
+    pub fn receive_all(mut self) -> impl Future<Error = Error, Item = impl Responder> {
+        let mut response = http::Response::new(());
+        *response.status_mut() = self.resp.status();
+        mem::swap(response.headers_mut(), self.resp.headers_mut());
+
+        let content_length = response
+            .headers()
+            .get("content-length")
+            .and_then(|h| h.to_str().ok())
+            .and_then(|s| s.parse::<usize>().ok())
+            .unwrap_or(0);
+
+        self.resp
+            .into_body()
+            .fold(Vec::with_capacity(content_length), |mut acc, chunk| {
+                acc.extend_from_slice(&*chunk);
+                Ok::<_, reqwest::Error>(acc)
+            }).map(move |chunks| response.map(|_| chunks))
+            .map_err(tsukuyomi::error::internal_server_error)
+    }
+}
+
+impl Responder for ProxyResponse {
+    type Body = tsukuyomi::output::ResponseBody;
+    type Error = Never;
+
+    fn respond_to(mut self, _: &mut Input<'_>) -> Result<http::Response<Self::Body>, Self::Error> {
+        let mut response = http::Response::new(());
+        *response.status_mut() = self.resp.status();
+        mem::swap(response.headers_mut(), self.resp.headers_mut());
+
+        let body_stream = tsukuyomi::output::ResponseBody::wrap_stream(self.resp.into_body());
+
+        Ok(response.map(|_| body_stream))
+    }
+}
+
+pub fn proxy_client(
+    client: reqwest::async::Client,
+) -> impl Extractor<Output = (Client,), Error = Error> {
+    extractor::extension::cloned()
+        .and(extractor::ready(|input| {
+            Ok::<_, Never>(input.headers().clone())
+        })).map(move |peer_addr, headers| Client {
+            client: client.clone(),
+            headers,
+            peer_addr,
+        })
+}
