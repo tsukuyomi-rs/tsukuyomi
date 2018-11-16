eda244c13481870bbbfaea49bdadfdd72c9ffdae
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-11-16 14:04:26 +0900

    disable example-openssl on Windows

--- examples/openssl/src/main.rs
+++ examples/openssl/src/main.rs
@@ -1,17 +1,27 @@
+#[cfg(not(windows))]
 extern crate openssl;
+#[cfg(not(windows))]
 extern crate tsukuyomi;
 
+#[cfg(windows)]
+fn main() {
+    println!("This example does not work on Windows platform.");
+}
+
+#[cfg(not(windows))]
 fn main() -> tsukuyomi::server::Result<()> {
-    let mut builder = openssl::ssl::SslAcceptor::mozilla_modern(openssl::ssl::SslMethod::tls())?;
-    builder.set_certificate_file("./private/cert.pem", openssl::ssl::SslFiletype::PEM)?;
-    builder.set_private_key_file("./private/key.pem", openssl::ssl::SslFiletype::PEM)?;
+    use openssl::ssl::{AlpnError, SslAcceptor, SslFiletype, SslMethod};
+
+    let mut builder = SslAcceptor::mozilla_modern(SslMethod::tls())?;
+    builder.set_certificate_file("./private/cert.pem", SslFiletype::PEM)?;
+    builder.set_private_key_file("./private/key.pem", SslFiletype::PEM)?;
     builder.set_alpn_protos(b"\x02h2\x08http/1.1")?;
     builder.set_alpn_select_callback(|_, protos| {
         const H2: &[u8] = b"\x02h2";
         if protos.windows(3).any(|window| window == H2) {
             Ok(b"h2")
         } else {
-            Err(openssl::ssl::AlpnError::NOACK)
+            Err(AlpnError::NOACK)
         }
     });
     let ssl_acceptor = builder.build();
