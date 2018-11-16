7470e7cabd530dd69b8ea828f299844ec1c5d974
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-11-17 03:29:54 +0900

    fix example-juniper

--- examples/juniper/src/main.rs
+++ examples/juniper/src/main.rs
@@ -9,7 +9,7 @@ mod context;
 mod schema;
 
 use std::sync::Arc;
-use tsukuyomi_juniper::executor::Executor;
+use tsukuyomi_juniper::Executor;
 
 fn main() -> tsukuyomi::server::Result<()> {
     // Extractor for extracting `Executor` for executing a GraphQL request from client.
@@ -27,7 +27,10 @@ fn main() -> tsukuyomi::server::Result<()> {
     tsukuyomi::app()
         .route(
             tsukuyomi::app::route!("/") //
-                .reply(tsukuyomi_juniper::graphiql("/graphql")),
+                .reply({
+                    let source = tsukuyomi_juniper::graphiql_source("/graphql");
+                    move || source.clone()
+                }),
         ) //
         .route(
             tsukuyomi::app::route!("/graphql", methods = [GET, POST])
