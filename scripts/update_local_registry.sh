fd15680bde617c4de7e8ef64b6c0ef9b88979bac
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-11-17 20:57:18 +0900

    update local registry

---
+++ scripts/update_local_registry.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+MANIFEST_DIR="$(cd $(dirname $BASH_SOURCE)/..; pwd)"
+
+echo "[regenerate Cargo.lock...]"
+cd
+(set -x; cargo generate-lockfile --locked --manifest-path=$MANIFEST_DIR/Cargo.toml)
+
+echo "[fetch local registry...]"
+cd $MANIFEST_DIR
+(set -x; cargo local-registry -s Cargo.lock .registry-index)
+(set -x; cp Cargo.lock .registry-index/Cargo.lock)
