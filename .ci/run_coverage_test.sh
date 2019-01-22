8de956eaf1c4aeef0ef873da4810e45888808d5e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-01-23 01:06:57 +0900

    switch the coverage measurement tool to tarpaulin

--- .ci/run_coverage_test.sh
+++ .ci/run_coverage_test.sh
@@ -4,8 +4,11 @@ DIR="$(cd $(dirname $BASH_SOURCE); pwd)"
 
 set -ex
 
-$DIR/run_kcov.py --all
-$DIR/run_kcov.py -p tsukuyomi --all-features
-$DIR/run_kcov.py -p tsukuyomi-session --all-features
+cargo tarpaulin --verbose --out Xml --all
+bash <(curl -s https://codecov.io/bash)
 
-bash <(curl -s https://codecov.io/bash) -K -s "$DIR/../target/cov"
+cargo tarpaulin --verbose --packages tsukuyomi --all-features
+bash <(curl -s https://codecov.io/bash)
+
+cargo tarpaulin --verbose --packages tsukuyomi-session --all-features
+bash <(curl -s https://codecov.io/bash)
